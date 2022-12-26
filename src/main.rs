6f4bcdf149e91e6f3587cd51a92dcae5be087c34
qub3s <g.eckardt@protonmail.com>
2022-12-26 22:25:08 +0100

    changed search pattern

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ fn kmp(t : bytes::Bytes, p : bytes::Bytes, mut result : Vec<i32>) ->Vec<i32> {
     let mut j = -1;
     ppj[0] = -1;
 
+    // building the table (ppj)
     while i < p.len(){
         while j >= 0 && p[i] != p[j as usize]{
             j = ppj[j as usize];
@@ -20,6 +21,7 @@ fn kmp(t : bytes::Bytes, p : bytes::Bytes, mut result : Vec<i32>) ->Vec<i32> {
     i = 0;
     j = 0;
 
+    // searching for the word in the text
     while i < t.len() as usize{
         while j >= 0 && t[i] != p[j as usize]{
             j = ppj[j as usize];
@@ -28,7 +30,7 @@ fn kmp(t : bytes::Bytes, p : bytes::Bytes, mut result : Vec<i32>) ->Vec<i32> {
         j = j + 1;
 
         if j == p.len() as i32{
-            result.push(i as i32);
+            result.push(i as i32);              // writing the position of the word in the result vector
             j = ppj[j as usize];
         }
     }
@@ -36,5 +38,5 @@ fn kmp(t : bytes::Bytes, p : bytes::Bytes, mut result : Vec<i32>) ->Vec<i32> {
 }
 
 fn main(){
-    print!( "{:?}", kmp(bytes::Bytes::from_static(b"lol das ist aber nicht schlecht, lol aber auch nicht gut lol lll"), bytes::Bytes::from_static(b"alalal"), Vec::new() ) );   
+    print!( "{:?}", kmp(bytes::Bytes::from_static(b"this is some random text where i will search for the char combination om"), bytes::Bytes::from_static(b"om"), Vec::new() ) );   
 }
